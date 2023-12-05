d4382b43b54cc9d3c7ca02c597ec580ee6125248
Ruslan Shchetsko <r.shetchko@sdgroup.ai>
2023-12-05 10:50:25 +0100

    Revert "Squashed commit of the following:"
    This reverts commit d00e4f9970548e98d781f3c71b493a16a653900b.

--- metrics.go
+++ metrics.go
@@ -1,37 +1,38 @@
 package metrics
 
 import (
+	"strconv"
+
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func NewCounterVec(name, desc string, labels []string) *prometheus.CounterVec {
+type incomingRequestCounter struct {
+	appName string
+	*prometheus.CounterVec
+}
+
+func NewIncomingRequestCounter(appName string) incomingRequestCounter { //nolint: revive
 	counterVec := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: name,
-			Help: desc,
+			Name: "rc_ml_http_requests_total",
+			Help: "Total number of incoming requests",
 		},
-		labels,
+		[]string{"statusCode", "method", "path", "appName"},
 	)
 
 	prometheus.MustRegister(
 		counterVec,
 	)
 
-	return counterVec
+	return incomingRequestCounter{
+		appName:    appName,
+		CounterVec: counterVec,
+	}
 }
 
-func NewHistogramVec(name, desc string, labels []string) *prometheus.HistogramVec {
-	histogramVec := prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name: name,
-			Help: desc,
-		},
-		labels,
-	)
-
-	prometheus.MustRegister(
-		histogramVec,
-	)
-
-	return histogramVec
+func (c incomingRequestCounter) ObserveIncomingRequests(
+	statusCode int,
+	method, path string,
+) {
+	c.WithLabelValues(strconv.Itoa(statusCode), method, path, c.appName).Inc()
 }
